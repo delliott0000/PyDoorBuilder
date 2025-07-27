fb5758b5621dda934102cd1c857290b50a34905c
Delliott <dlelliott100@outlook.com>
2025-07-27 18:15:52 +0100

    state from_json method (implement later)

--- Common/state.py
+++ Common/state.py
@@ -3,12 +3,15 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, Self
 
 __all__ = ("State",)
 
 
 class State:
     __slots__ = ()
+
+    @classmethod
+    def from_json(cls, json: dict[str, Any], /) -> Self: ...
 
     def to_json(self) -> dict[str, Any]: ...
