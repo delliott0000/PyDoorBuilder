2d8ef2c0b1e10c651950d18cda57544e17f31721
Delliott <dlelliott100@outlook.com>
2025-08-01 11:42:51 +0100

    refactor Route to avoid multiple config reads

--- Common/route.py
+++ Common/route.py
@@ -4,34 +4,46 @@ from abc import ABC
 from typing import TYPE_CHECKING
 from urllib.parse import quote
 
-from .config import global_config
-
 if TYPE_CHECKING:
-    from typing import ClassVar
+    from typing import Any, ClassVar
 
-__all__ = ("Route", "HTTPRoute", "WebSocketRoute")
+__all__ = ("build_base", "setup_routes", "Route", "HTTPRoute", "WebSocketRoute")
 
 
-domain = global_config["api"]["domain"]
-secure = global_config["api"]["secure"]
-local = global_config["api"]["local"]
+def build_base(protocol: str, domain: str, use_secure: bool, /) -> str:
+    return f"{protocol}{'s' if use_secure else ''}://{domain}"
 
-use_secure = secure and not local
 
-if local:
-    host = global_config["server"]["api"]["host"]
-    port = global_config["server"]["api"]["port"]
-    resolved_domain = f"{host}:{port}"
-else:
-    resolved_domain = domain
+def setup_routes(config: dict[str, Any], /) -> None:
+    domain = config["api"]["domain"]
+    secure = config["api"]["secure"]
+    local = config["api"]["local"]
+
+    use_secure = secure and not local
+
+    if local:
+        host = config["server"]["api"]["host"]
+        port = config["server"]["api"]["port"]
+        resolved_domain = f"{host}:{port}"
+    else:
+        resolved_domain = domain
+
+    HTTPRoute.BASE = build_base("http", resolved_domain, use_secure)
+    WebSocketRoute.BASE = build_base("ws", resolved_domain, use_secure)
 
 
 class Route(ABC):
     BASE: ClassVar[str] = ""
 
     def __init__(self, path: str, /, **kwargs: str):
-        if self.BASE == "":
-            raise ValueError("Route must have a base.")
+        if type(self) is Route:
+            raise NotImplementedError(
+                "Route is an abstract base class and cannot be instantiated directly."
+            )
+        elif self.BASE == "":
+            raise RuntimeError(
+                "Route instances must have a base. Did you forget to call setup_routes(...)?"
+            )
 
         self.__path: str = path
         self.__kwargs: dict[str, str] = {k: quote(v, safe="") for k, v in kwargs.items()}
@@ -45,8 +57,8 @@ class Route(ABC):
 
 
 class HTTPRoute(Route):
-    BASE: ClassVar[str] = f"http{'s' if use_secure else ''}://{resolved_domain}"
+    pass
 
 
 class WebSocketRoute(Route):
-    BASE: ClassVar[str] = f"ws{'s' if use_secure else ''}://{resolved_domain}"
+    pass
