3155c3858ab269cdca90e8a79d45815b8d4d7dbb
Delliott <dlelliott100@outlook.com>
2025-10-04 14:14:12 +0100

    add stuff to Team definition

--- Common/team.py
+++ Common/team.py
@@ -1,10 +1,29 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from .bases import ComparesIDABC, ComparesIDMixin
+
+if TYPE_CHECKING:
+    from asyncpg import Record
 
 __all__ = ("Team",)
 
 
 class Team(ComparesIDMixin, ComparesIDABC):
-    def __init__(self): ...
+    __slots__ = ("_id", "_name")
+
+    def __init__(self, team_record: Record | dict, /):
+        self._id = team_record["id"]
+        self._name = team_record["name"]
+
+    def __str__(self):
+        return self._name
 
     @property
-    def id(self) -> int: ...
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
