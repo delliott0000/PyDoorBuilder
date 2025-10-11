7c084c7700aaded34c71af700aa5ab703acc4399
Delliott <dlelliott100@outlook.com>
2025-10-11 01:05:10 +0100

    Company.to_json

--- Common/company.py
+++ Common/company.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 from .bases import ComparesIDABC, ComparesIDMixin
 
 if TYPE_CHECKING:
+    from typing import Any
+
     from asyncpg import Record
 
 __all__ = ("Company",)
@@ -25,3 +27,9 @@ class Company(ComparesIDMixin, ComparesIDABC):
     @property
     def name(self) -> str:
         return self._name
+
+    def to_json(self) -> dict[str, Any]:
+        return {
+            "id": self._id,
+            "name": self._name,
+        }
