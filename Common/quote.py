d1280b32eb5febdcebef3c168b0997e0307bd4db
Delliott <dlelliott100@outlook.com>
2025-11-21 08:46:28 +0000

    start to implement the base Quote model

--- Common/quote.py
+++ Common/quote.py
@@ -7,6 +7,8 @@ from .bases import ComparesIDFormattedABC, ComparesIDFormattedMixin
 if TYPE_CHECKING:
     from typing import Any
 
+    from asyncpg import Record
+
     from .resource import ResourceJSONVersion
     from .user import User
 
@@ -14,16 +16,22 @@ __all__ = ("Quote",)
 
 
 class Quote(ComparesIDFormattedMixin, ComparesIDFormattedABC):
-    __slots__ = ()
+    __slots__ = ("_id", "_owner")
+
+    def __init__(self, quote_record: Record | dict, owner: User, /):
+        self._id = quote_record["id"]
+        self._owner = owner
 
     @property
-    def id(self) -> int: ...
+    def id(self) -> int:
+        return self._id
 
     @property
     def formatted_id(self) -> str:
         return f"SQ{self.id:08d}"
 
     @property
-    def owner(self) -> User: ...
+    def owner(self) -> User:
+        return self._owner
 
     def to_json(self, *, version: ResourceJSONVersion) -> dict[str, Any]: ...
