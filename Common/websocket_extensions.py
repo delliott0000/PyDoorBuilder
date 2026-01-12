eefd608ad4f95a394dd815081bc63eff231ddb88
Delliott <dlelliott100@outlook.com>
2026-01-12 18:08:31 +0000

    implement websocket message rate limiter

--- Common/websocket_extensions.py
+++ Common/websocket_extensions.py
@@ -3,10 +3,15 @@ from __future__ import annotations
 from enum import IntEnum
 from typing import TYPE_CHECKING
 
+from aiohttp import WSCloseCode
 from aiohttp.web import WebSocketResponse
+
+from .utils import check_ratelimit
 
 if TYPE_CHECKING:
     from typing import Any
+
+    from aiohttp import WSMessage
 
 __all__ = ("CustomWSCloseCode", "CustomWSResponse")
 
@@ -32,3 +37,16 @@ class CustomWSResponse(WebSocketResponse):
         self.__limit = limit
         self.__interval = interval
         self.__hits = []
+
+    async def __anext__(self) -> WSMessage:
+        message = await super().__anext__()
+
+        try:
+            self.__hits = check_ratelimit(
+                self.__hits, limit=self.__limit, interval=self.__interval
+            )
+        except RuntimeError:
+            await self.close(code=WSCloseCode.POLICY_VIOLATION)
+            raise StopAsyncIteration
+
+        return message
